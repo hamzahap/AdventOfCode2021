afa7f8f75c09e60f866fd3a78ba259c1d1491788
hamzahap <hamzahqatar123@gmail.com>
2021-12-07 12:51:34 -0330

    Use numpy

--- Day 6/main.py
+++ Day 6/main.py
@@ -1,15 +1,16 @@
 import sys
+import numpy as np
 
-file = sys.argv[1] if len(sys.argv)>1 else 'inputexample.txt'
+file = sys.argv[1] if len(sys.argv)>1 else 'input.txt'
 
 for line in open(file):
-  fishyboi = list(line.split(","))
-fishyboi = [int(x) for x in fishyboi]
+  fishyboi = np.array(line.strip().split(","))
+fishyboi = fishyboi.astype(int)
 for x in range (256):
-  for y in range(len(fishyboi)):
+  for y in range(fishyboi.size):
     fishyboi[y] -= 1
     if fishyboi[y] == -1:
-      fishyboi.append(8)
+      fishyboi = np.append(fishyboi,8)
       fishyboi[y] = 6
 
-print(len(fishyboi))
+print(fishyboi.size)
